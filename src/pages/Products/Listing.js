8fb7298540f44ac4f1cff03114294f73b4750f44
Ruslan Wanandi <devruslan03@gmail.com>
2021-08-25 10:55:11 +0700

    feat: update prodcuts card listing

--- src/pages/Products/Listing.js
+++ src/pages/Products/Listing.js
@@ -18,15 +18,14 @@ const Listing = () => {
   };
 
   const addToChart = (product) => {
-    if(!isLogin) {
-      history.push('/login')
+    if (!isLogin) {
+      history.push("/login");
     }
-    delete product['id']
+    delete product["id"];
     addCart(uid, {
       ...product,
       Qty: 1,
-    })
-    .catch((err) => console.log(err));
+    }).catch((err) => console.log(err));
   };
 
   useEffect(() => {
@@ -46,60 +45,65 @@ const Listing = () => {
       <h1 className="text-3xl font-semibold">Semua Produk</h1>
       <p>Semua produk yang anda cari ada disini!</p>
 
-      {loadingProduct && "Sebentar, lg disiapin :D"}
+      <div className="flex flex-wrap justify-between gap-1 w-full mt-8">
+        {loadingProduct && "Sebentar, lg disiapin :D"}
 
-      {productList &&
-        productList.map((product) => {
-          return (
-            <div className="my-8" key={product.id}>
-              <div className="flex max-w-md bg-white shadow-md rounded-lg overflow-hidden">
-                <div
-                  className="w-1/3 bg-cover"
-                  style={{
-                    'backgroundImage': `url(${product.ProductThumbnail})`,
-                  }}
-                ></div>
-                <div className="w-2/3 p-4">
-                  <h1 className="text-gray-900 font-bold text-2xl">
-                    {product.ProductName}
-                  </h1>
-                  <p className="mt-2 text-gray-600 text-sm">
-                    {product.ProductDescription}
-                  </p>
-                  <div className="flex item-center justify-between mt-3">
-                    <h1 className="text-gray-700 font-bold text-xl">
-                      {kFormatter(product.ProductPrice)}
+        {productList &&
+          productList.map((product) => {
+            return (
+              <div className="my-4" key={product.id}>
+                <div className="flex max-w-md bg-white shadow-md rounded-lg overflow-hidden">
+                  <div
+                    className="w-1/3 bg-cover"
+                    style={{
+                      backgroundImage: `url(${product.ProductThumbnail})`,
+                    }}
+                  ></div>
+                  <div className="w-2/3 p-4">
+                    <h1 title={product.ProductName} className="text-gray-900 font-bold text-2xl truncate">
+                      {product.ProductName}
                     </h1>
-                    <div className="inline-flex">
-                      <Link
-                        to={`/p/${product.id}`}
-                        className="px-3 py-2 mr-1 bg-gray-800 text-white text-xs font-bold uppercase rounded"
-                      >
-                        Detail
-                      </Link>
-                      <button onClick={() => addToChart(product)} className="px-3 py-2 bg-yellow-500 text-white text-xs font-bold uppercase rounded">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-5 w-5"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
+                    <p title={product.ProductDescription} className="mt-2 text-gray-600 text-sm truncate items-center justify-center">
+                      {product.ProductDescription}
+                    </p>
+                    <div className="flex item-center justify-between mt-3">
+                      <h1 className="text-gray-700 font-bold text-xl">
+                        {kFormatter(product.ProductPrice)}
+                      </h1>
+                      <div className="inline-flex">
+                        <Link
+                          to={`/p/${product.id}`}
+                          className="px-3 py-2 mr-1 bg-gray-800 text-white text-xs font-bold uppercase rounded"
                         >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                          />
-                        </svg>
-                      </button>
+                          Detail
+                        </Link>
+                        <button
+                          onClick={() => addToChart(product)}
+                          className="px-3 py-2 bg-yellow-500 text-white text-xs font-bold uppercase rounded"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                            />
+                          </svg>
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+      </div>
     </div>
   );
 };
